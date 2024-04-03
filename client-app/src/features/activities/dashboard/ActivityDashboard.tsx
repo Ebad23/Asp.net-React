d8df09dcf3986b4c18a7ffbe75354b5db9d7e012
Ebad <ebadahmed306@gmail.com>
2024-04-03 15:43:20 +0500

    Create CRUD in React

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,46 @@
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../dashboard/ActivityDetail";
+import ActivityForm from "../dashboard/Form/ActivityForm";
+import ActivityList from './ActivityList';
+
+interface Props {
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    editMode: boolean;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+}
+
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity,
+    cancelSelectActivity, openForm, closeForm, editMode, createOrEdit, deleteActivity }: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList 
+                    activities={activities} 
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity} 
+                />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    />}
+                {editMode &&
+                    <ActivityForm 
+                        closeForm={closeForm} 
+                        activity={selectedActivity} 
+                        createOrEdit={createOrEdit} />}
+            </Grid.Column>
+
+        </Grid>
+    )
+}
